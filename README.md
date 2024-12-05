6ba6d75b1272b9710886052c62a3abcd09a50654
Hayato Asami <mail@has-on.info>
2024-12-06 00:19:16 +0900

    feat: formatter

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-## Install  
-  
-`brew install docker --cask` (Optional)  
-  
-## Run  
-  
-`docker-compose up -d`  
-  
-Now you can access the app at `http://localhost:8001`  
+## Install
+
+`brew install docker --cask` (Optional)
+
+## Run
+
+`docker-compose up -d`
+
+Now you can access the app at `http://localhost:8001`
---
+++ main-theme/single.php
@@ -0,0 +1,44 @@
+<?php
+get_header();
+?>
+
+<main >
+        <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) :
+                the_post(); // 投稿データを取得
+        ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header>
+                        <h1 ><?php the_title(); ?></h1>
+                        <div >
+                            <p>投稿日: <?php the_date(); ?> | 著者: <?php the_author(); ?></p>
+                        </div>
+                    </header>
+                    <div >
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+
+                <div >
+                    <div><?php previous_post_link( '%link', '← 前の記事' ); ?></div>
+                    <div><?php next_post_link( '%link', '次の記事 →' ); ?></div>
+                </div>
+
+                <?php
+                // コメント表示
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+                ?>
+        <?php
+            endwhile;
+        else :
+            echo '<p>投稿が見つかりません。</p>';
+        endif;
+        ?>
+    </main>
+
+<?php
+get_footer();
+?>
