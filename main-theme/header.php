e784322c5f2b06b0bb086b507381178b447192f9
Hayato Asami <mail@has-on.info>
2025-03-07 21:24:34 +0900

    feat: formatter

--- main-theme/header.php
+++ main-theme/header.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php wp_title( '｜', true, 'right' ); bloginfo('name');?></title>
-<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" />
-<?php wp_head(); ?>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php wp_title('｜', true, 'right');
+          bloginfo('name'); ?></title>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+  <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
