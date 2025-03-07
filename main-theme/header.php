4acef4e641a1f461d031881e72afd0fc5fe53ce8
Hayato Asami <mail@has-on.info>
2025-03-07 21:26:45 +0900

    fix: header link

--- main-theme/header.php
+++ main-theme/header.php
@@ -7,7 +7,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php wp_title('｜', true, 'right');
           bloginfo('name'); ?></title>
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
   <?php wp_head(); ?>
 </head>
 
