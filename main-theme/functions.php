71fc3d653028051a5c55058e03507000417dce2d
Hayato Asami <mail@has-on.info>
2025-08-12 00:25:41 +0900

    feat: address font-style

--- main-theme/functions.php
+++ main-theme/functions.php
@@ -4,7 +4,7 @@ function enqueue_styles()
 {
   wp_enqueue_style('ress', 'https://unpkg.com/ress/dist/ress.min.css');
   wp_enqueue_style('style', get_stylesheet_uri());
-  wp_enqueue_style('slick-style', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array('main-style'), '1.8.1');
+  wp_enqueue_style('slick-style', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css', array(), '1.8.1');
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
 
