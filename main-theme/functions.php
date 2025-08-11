5537a0db917fe9ce8de93139bd0ba5df477e39d2
Hayato Asami <mail@has-on.info>
2025-08-11 17:47:35 +0900

    fix

--- main-theme/functions.php
+++ main-theme/functions.php
@@ -2,8 +2,7 @@
 
 function enqueue_styles()
 {
-  wp_enqueue_style('style', 'https://unpkg.com/ress/dist/ress.min.css');
-
+  wp_enqueue_style('ress', 'https://unpkg.com/ress/dist/ress.min.css');
   wp_enqueue_style('style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'enqueue_styles');
